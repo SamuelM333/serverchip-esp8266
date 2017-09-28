4f291bccae9254391823d0028676f9496c66ef5d
Samuel Murillo <samuelmurillo333@gmail.com>
2017-09-28 00:41:20 -0400

    Rename handle_get_port_status to handle_get_ports_status

--- src/ws_events.h
+++ src/ws_events.h
@@ -1,7 +1,7 @@
 #ifndef SERVERCHIP_ESP8266_WS_EVENTS_H
 #define SERVERCHIP_ESP8266_WS_EVENTS_H
 
-void handle_get_port_status(const char *payload, size_t length);
+void handle_get_ports_status(const char *payload, size_t length);
 void handle_run_task_request_microchip(const char *payload, size_t length);
 void handle_microchip_connected_ack(const char *payload, size_t length);
 
